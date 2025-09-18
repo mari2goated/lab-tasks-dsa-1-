1f4159a520bdfd06792c16af181fdd1b9f301e71
Mariam Mateen <mariam.mateen.018@gmail.com>
2025-09-18 13:56:17 +0500

    Update task7.java

--- task7.java
+++ task7.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
 public class task7 {
+    public static void main(String[] args) {
+        System.out.println("enter a number");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int a = 0;
+        int b = 1;
+        int c = 0;
+
+        if (n==0){
+            c =a ;
+        }
+        else if (n == 1){
+            c = b;
+        }
+        else{ 
+            for (int i=2; i <= n; i++){ 
+                c = a+b;
+                a = b;
+                b = c;
+            }
+        }
+        System.out.println(n + "=" + c);
+        
+    }
     
 }
